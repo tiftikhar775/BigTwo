6c1f0526df8812b1f19cfb3f7828fb37e9ca888e
tiftikhar775 <tiftikhar775@gmail.com>
2025-01-29 17:35:37 -0600

    refactored code, added skeleton for game menu and game instructions menu

--- src/main/java/org/example/MenuDisplays.java
+++ src/main/java/org/example/MenuDisplays.java
@@ -5,16 +5,38 @@ import java.util.Scanner;
 class MenuDisplays {
 
     public static void mainMenuDisplay() {
-        int option;
         Scanner scanner = new Scanner(System.in);
-
+        int option;
+        System.out.println("Welcome to Talha's Big 2!");
         do {
-            System.out.println("Big Two!");
-            System.out.println("Select Number of Players");
-            System.out.println("1. Two Players");
-            System.out.println("2. Four Players");
+            System.out.println("1. Play a round");
+            System.out.println("2. How to play");
             option = scanner.nextInt();
         } while (option < 1 || option > 2);
+
+        if (option == 1) {
+            gameMenuDisplay();
+        } else {
+            instructionsMenu();
+        }
+
+    }
+
+    public static void instructionsMenu() {
+
+
+
+    }
+
+
+
+
+    public static void gameMenuDisplay() {
+        System.out.println("\nGame Menu");
+        System.out.println("1. Play Singles");
+        System.out.println("2. Play Doubles");
+        System.out.println("3. Play Triples");
+        System.out.println("4. Play 5-Card");
     }
 
 
