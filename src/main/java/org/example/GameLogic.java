d3e2e3536e0eb2bcc60d3b7530160902df17d368
tiftikhar775 <tiftikhar775@gmail.com>
2025-01-29 17:37:09 -0600

    renamed class to gameLogic and changed logic for cardHand method to return an array list instead

--- src/main/java/org/example/GameLogic.java
+++ src/main/java/org/example/GameLogic.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import java.util.Random;
+import java.util.ArrayList;
 
-public class Cards {
+public class GameLogic {
 
-    public static void generateCardHand(int numOfCards) {
+    public static ArrayList<String> generateCardHand() {
 
-        // initialize card deck array of suit arrays and card ranks [4]x[13]
+        // initialize card deck 2d array consisting of suit arrays and card ranks [4]x[13]
         String[][] cardDeck = {
                 { "2◆", "3◆", "4◆", "5◆", "6◆", "7◆", "8◆", "9◆", "10◆", "J◆", "Q◆", "K◆", "A◆"},
                 { "2♧", "3♧", "4♧", "5♧", "6♧", "7♧", "8♧", "9♧", "10♧", "J♧", "Q♧", "K♧", "A♧"},
@@ -16,8 +17,10 @@ public class Cards {
 
         Random random = new Random();
 
-        // do this for the number of cards specified
-        for (int i = 0; i < numOfCards; i++) {
+        ArrayList<String> cardHand = new ArrayList<>(); // initialize array list card hand
+
+        // do this for half the deck so cards are divided evenly between player and cpu
+        for (int i = 0; i < 52/2; i++) {
             int cardSuit;
             int cardRank;
             // only generate card suit and card rank that has not been marked
@@ -26,11 +29,16 @@ public class Cards {
                 cardRank = random.nextInt(cardDeck[cardSuit].length); // select a random element from the randomly selected suit array
             } while (cardDeck[cardSuit][cardRank] == null);
 
-            String randomCard = cardDeck[cardSuit][cardRank]; // store the card suit and card rank combination
-            System.out.print(randomCard + " ");
+
+             cardHand.add(cardDeck[cardSuit][cardRank]);
+
+            //String randomCard = cardDeck[cardSuit][cardRank]; // store the card suit and card rank combination
+            //System.out.print(randomCard + " ");
 
             cardDeck[cardSuit][cardRank] = null; // mark this cardSuit and cardRank combination so that it won't get selected again
         }
+
+        return cardHand;
 
     }
 
