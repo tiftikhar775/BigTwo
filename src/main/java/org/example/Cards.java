0adb258acf12cbc77097016e7b0dcd88dd6d1d6f
tiftikhar775 <tiftikhar775@gmail.com>
2025-01-29 16:59:40 -0600

    Renamed variables and added better comments explaining logic

--- src/main/java/org/example/Cards.java
+++ src/main/java/org/example/Cards.java
@@ -6,7 +6,7 @@ public class Cards {
 
     public static void generateCardHand(int numOfCards) {
 
-        // init 2d array [4]x[13]
+        // initialize card deck array of suit arrays and card ranks [4]x[13]
         String[][] cardDeck = {
                 { "2◆", "3◆", "4◆", "5◆", "6◆", "7◆", "8◆", "9◆", "10◆", "J◆", "Q◆", "K◆", "A◆"},
                 { "2♧", "3♧", "4♧", "5♧", "6♧", "7♧", "8♧", "9♧", "10♧", "J♧", "Q♧", "K♧", "A♧"},
@@ -18,17 +18,18 @@ public class Cards {
 
         // do this for the number of cards specified
         for (int i = 0; i < numOfCards; i++) {
-            int randomSuit;
-            int randomRank;
-            // only generate card suit and rank that has not been used before
+            int cardSuit;
+            int cardRank;
+            // only generate card suit and card rank that has not been marked
             do {
-                randomSuit = random.nextInt(cardDeck.length); // get random element from 2d array row
-                randomRank = random.nextInt(cardDeck[randomSuit].length); // get random element from randomly selected row
-            } while (cardDeck[randomSuit][randomRank] == null);
+                cardSuit = random.nextInt(cardDeck.length); // select a suit array randomly from cardDeck array
+                cardRank = random.nextInt(cardDeck[cardSuit].length); // select a random element from the randomly selected suit array
+            } while (cardDeck[cardSuit][cardRank] == null);
 
-            String randomCard = cardDeck[randomSuit][randomRank]; // store random 2d array index
+            String randomCard = cardDeck[cardSuit][cardRank]; // store the card suit and card rank combination
             System.out.print(randomCard + " ");
-            cardDeck[randomSuit][randomRank] = null; // mark this card so it won't get selected again
+
+            cardDeck[cardSuit][cardRank] = null; // mark this cardSuit and cardRank combination so that it won't get selected again
         }
 
     }
